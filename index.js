6203cb8a6104106ba0abb6dc3d42c138aad33d7f
Lucas Didur <ldidur@icloud.com>
2020-10-25 12:18:13 -0300

    feat: added support for graphql

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  mountGraphQL: true,
-  mountPlayground: true,
-  graphQLSchema: __dirname + '/cloud/schema.graphql',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
